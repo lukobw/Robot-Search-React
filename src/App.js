9831e7a205adc837c55d935a2ebbf3386002057c
lukobw <25585115+lukobw@users.noreply.github.com>
2021-02-12 10:32:24 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import CardList from './CardList';
+import {robots} from './robots';
+import SearchBox from './SearchBox';
+import './App.css';
+
+ class App extends Component {
+     constructor(props) {
+        super(props);
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+     onSearchChange = (event) => {
+         this.setState({searchField: event.target.value})
+         
+     console.log(event.target.value);
+    }
+     render() {
+         const filteredRobots = this.state.robots.filter(robots => {
+             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+         });
+        return (
+            <div className="tc">
+                <h1 className="f1">Robot Search</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Card.css'
+
+const Card = ({id, name, email}) => {
+    return (
+      <div className='tc dib bg-light-green br3 ma2 grow bw2 shadow-5'>
+          <h1>{id}</h1>
+          <img src={`https://robohash.org/${id}}?size=200x200`} alt=''/>
+          <div>
+              <h2>{name}</h2>
+              <p>{email}</p>
+          </div>
+      </div>  
+    );
+}
+
+export default Card;
